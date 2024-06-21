8e92dbe89d252a439543e24781e41f5f9eb147df
dariosaric94 <saricdario94@gmail.com>
2024-06-21 22:18:18 +0200

    UPDATE

--- src/components/Grid/Grid.tsx
+++ src/components/Grid/Grid.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import styles from "./grid.module.scss";
+import React from 'react';
+import styles from './grid.module.scss';
 
 interface ColumnProps extends React.HTMLAttributes<HTMLDivElement> {
   children?: React.ReactNode;
@@ -8,30 +8,39 @@ interface ColumnProps extends React.HTMLAttributes<HTMLDivElement> {
    */
   xs?: number;
   /**
-   * Small screen size 0 to 600px
+   * Small screen size 0px to 600px
    */
   sm?: number;
   /**
-   * Small medium screen size 600 to 960px
+   * Small medium screen size 600px to 960px
    */
   md?: number;
   /**
-   * Medium screen size 960 to 1280px
+   * Medium screen size 960px to 1280px
    */
   lg?: number;
   /**
-   * Large screen size 1280+ and above
+   * Large screen size 1280px to 1920px and above
+   */
+  xl?: number;
+  /**
+   * Large screen size 1920px+ and above
    */
 }
 
 export const Column: React.FC<ColumnProps> = ({ children, xs, sm, md, lg, ...rest }) => {
-  const classes = [xs ? styles[`xs-${xs}`] : "", sm ? styles[`sm-${sm}`] : "", md ? styles[`md-${md}`] : "", lg ? styles[`lg-${lg}`] : ""].filter(Boolean).join(" ");
+  const classes = [
+    xs ? styles[`xs-${xs}`] : '',
+    sm ? styles[`sm-${sm}`] : '',
+    md ? styles[`md-${md}`] : '',
+    lg ? styles[`lg-${lg}`] : '',
+  ]
+    .filter(Boolean)
+    .join(' ');
 
   return (
     <div className={`${styles.gridItem} ${classes}`}>
-      <div {...rest}>
-        {children}as
-      </div>
+      <div {...rest}>{children}as</div>
     </div>
   );
 };
@@ -50,7 +59,7 @@ interface RowProps extends React.HTMLAttributes<HTMLDivElement> {
 export const Row: React.FC<RowProps> = ({ className, children, gap = 0, ...rest }) => {
   let style = {};
   if (gap !== undefined) {
-    style = { "--gap": `${gap}px` };
+    style = { '--gap': `${gap}px` };
   }
   return (
     <div className={`${styles.gridContainer} ${className}`} {...rest} style={style}>
